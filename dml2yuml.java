86e157f30a50819432745944407748a21a244e35
Pedro Reis Santos <reis.santos@tecnico.ulisboa.pt>
2015-10-23 18:52:26 +0100

    added MANIFEST

--- dml2yuml.java
+++ dml2yuml.java
@@ -3,6 +3,7 @@
  *
  * compile: antlr4 dml.g4; javac -cp antlr-4.5-complete.jar:. *.java
  * run: java -cp antlr-4.5-complete.jar:. dml2yuml ex.dml
+ * jar: jar -cfm dml2yuml.jar MANIFEST.MF *.class dml2yuml.java dml.g4 antlr-4.5-complete.jar
  * reis.santos@tecnico.ulisboa.pt (C)21oct2015
  */
 
